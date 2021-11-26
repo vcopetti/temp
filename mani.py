ea38ed8ebe4271a91ee523163f275477a3751135
vcopetti <vcopetti@gmail.com>
2021-11-26 17:30:26 -0300

    teste 01

--- mani.py
+++ mani.py
@@ -1,4 +1,15 @@
 #  Apenas um exemplo para usar no git
 
+class Abc(object):
+    id_abc = 0
+    nome = ""
+
+
 def princ():
     print('Proc Princ')
+
+
+print('Vou chamar a func:')
+princ()
+
+x = Abc()
